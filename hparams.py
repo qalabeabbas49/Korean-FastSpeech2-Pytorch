18a0879262c8ae904ade54607e7e17fcf2714071
QALAB E ABBAS <44332537+qalabeabbas49@users.noreply.github.com>
2021-10-06 18:34:01 +0900

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -1,7 +1,7 @@
 import os
 ### kss ###
 dataset = "kss"
-data_path = os.path.join("/home/centos/qalab_KFS2/", dataset)
+data_path = os.path.join("/home/centos/qalab_KFS2/", dataset) #enter the directory where your data is present
 meta_name = "transcript.v.1.4.txt"	# "transcript.v.1.4.txt" or "transcript.v.1.3.txt" 
 textgrid_name = "TextGrid"
 
